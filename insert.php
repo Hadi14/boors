afdf44a428d750e5b11d1dad10d341bd2239e28c
unknown <HadiHashemi@H_Hashemi.chb.local>
2020-02-09 14:07:49 +0330

    com

--- insert.php
+++ insert.php
@@ -2,8 +2,10 @@
 
 include_once 'dbcon.php';
 if(isset($_POST['btn'])) {
-    if (empty($_POST['btn']))
+    if (empty($_FILES['file']['name']))
+{
     echo "File is not Selected";
+}
     else
     {
     include_once 'simplexlsx.class.php';
@@ -17,13 +19,15 @@ if(isset($_POST['btn'])) {
             }
         }
         }
-    }
+    
    
-}
+
 foreach($array as $item) {
  
             mysqli_query($db,"INSERT INTO `boorsupl` (`nemadid`, `volume`, `value`, `endprice`, `endperc`, `priceend`, `endchange`, `min`, `max`, `inoutpric`, `dist3mon`, `powerbyue`, `output`, `ndate`) 
             VALUES ('{$item[0]}', '{$item[1]}', '{$item[2]}', '{$item[3]}', '{$item[4]}', '{$item[5]}', '{$item[6]}', '{$item[7]}', '{$item[8]}', '{$item[9]}', '{$item[10]}', '{$item[11]}', '{$item[12]}', '{$item[13]}')");
 
+}
+}
 }
 ?>
