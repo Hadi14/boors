88cf74e0783bbd9596e517558f79837a98ae03de
unknown <HadiHashemi@H_Hashemi.chb.local>
2020-02-09 12:42:15 +0330

    up

--- insert.php
+++ insert.php
@@ -1,6 +1,11 @@
 <?php
+
 include_once 'dbcon.php';
 if(isset($_POST['btn'])) {
+    if (empty($_POST['btn']))
+    echo "File is not Selected";
+    else
+    {
     include_once 'simplexlsx.class.php';
     $xlsx = new SimpleXLSX($_FILES['file']['tmp_name']);
     list($cols,) = $xlsx->dimension();
@@ -11,11 +16,14 @@ if(isset($_POST['btn'])) {
                 $array[$k] = $r;
             }
         }
+        }
     }
+   
 }
 foreach($array as $item) {
-    /*mysqli_query($db,"INSERT INTO `nemadid` (`nid`,`nname`)
-            VALUES('{$item[0]}','{$item[1]}')");*/
-            mysqli_query($db,"INSERT INTO `boorsupl`(`nemadid`, `volume`, `value`, `endprice`, `endperc`, `priceend`, `endchange`, `min`, `max`, `inoutpric`, `dist3mon`, `powerbyue`, `output`, `ndate`) VALUES ('{$item[0]}','{$item[1]}'),'{$item[2]}','{$item[3]}'),'{$item[4]}','{$item[5]}'),'{$item[6]}','{$item[7]}'),'{$item[8]}','{$item[9]}'),'{$item[10]}','{$item[11]}'),'{$item[12]}','{$item[13]}')"
+ 
+            mysqli_query($db,"INSERT INTO `boorsupl` (`nemadid`, `volume`, `value`, `endprice`, `endperc`, `priceend`, `endchange`, `min`, `max`, `inoutpric`, `dist3mon`, `powerbyue`, `output`, `ndate`) 
+            VALUES ('{$item[0]}', '{$item[1]}', '{$item[2]}', '{$item[3]}', '{$item[4]}', '{$item[5]}', '{$item[6]}', '{$item[7]}', '{$item[8]}', '{$item[9]}', '{$item[10]}', '{$item[11]}', '{$item[12]}', '{$item[13]}')");
+
 }
 ?>
