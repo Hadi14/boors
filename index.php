2c9d0df5a39859465fc9cdded77164aa36dee2b6
hadi14 <asusn43@yahoo.com>
2020-02-10 19:42:02 +0330

    up

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
     <input type="file" name="file">
     <input type="submit" value="send" name="btn">
 </form>
+
+
+
+<form method="post" action="insert2.php" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="send" name="btn2">
+</form>
