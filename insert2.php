31cdc322316a0ebbf1b1b5c0cffc134de263f660
unknown <HadiHashemi@H_Hashemi.chb.local>
2020-02-13 13:18:11 +0330

    add

--- insert2.php
+++ insert2.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once 'dbcon.php';
-if (isset($_POST['btn']))
+if (isset($_POST['btn2']))
  {
     if (empty($_FILES['file']['name'])) 
     {
@@ -17,9 +17,11 @@ if (isset($_POST['btn']))
             for ($i = 1; $i <= $cols; $i++) 
             {
                 if ($r[1] != "" && $i <= 4)
-                 {
+                {
                     $array[$k] = $r;
                 }
+                //$i++;
+                echo $i." ";
             }
         }
 
@@ -27,9 +29,8 @@ if (isset($_POST['btn']))
 
         foreach ($array as $item) 
         {
-
-            mysqli_query($db, "INSERT INTO `boorsupl` (`nemadid`, `volume`, `value`, `endprice`, `endperc`, `priceend`, `endchange`, `min`, `max`, `inoutpric`, `dist3mon`, `powerbyue`, `output`, `ndate`) 
-            VALUES ('{$item[0]}', '{$item[1]}', '{$item[2]}', '{$item[3]}', '{$item[4]}', '{$item[5]}', '{$item[6]}', '{$item[7]}', '{$item[8]}', '{$item[9]}', '{$item[10]}', '{$item[11]}', '{$item[12]}', '{$item[13]}')");
+            mysqli_query($db, "INSERT INTO `boorsupl`(`nemadid`, `volume`, `value`, `endprice`, `endperc`, `priceend`, `endchange`, `min`, `max`, `inoutpric`, `dist3mon`, `powerbyue`) VALUES
+             ('{$item[0]}','{$item[1]}','{$item[2]}','{$item[3]}','{$item[4]}','{$item[5]}','{$item[6]}','{$item[7]}','{$item[8]}','{$item[9]}','{$item[10]}','{$item[11]}')");
         }
     }
 }
