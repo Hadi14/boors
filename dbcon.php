fd3a492825d9ac50341ef8268d89dd13e45431f7
Hadi H <asusn43@yahoo.com>
2020-02-18 08:49:29 -0800

    d

--- dbcon.php
+++ dbcon.php
@@ -1,4 +1,5 @@
 <?php
-$db=mysqli_connect('localhost','root','','boors')
-
+$db=mysqli_connect('localhost','root','','boors');
+mysqli_query($db, "SET NAMES utf8");
+//mysqli_query($db,"SET CHARACTER SET utf8");
 ?>
--- index.php
+++ index.php
@@ -11,3 +11,22 @@
     <input type="file" name="file">
     <input type="submit" value="send" name="btn2">
 </form>
+<br>
+<form method="post" action="index.php" enctype="multipart/form-data">
+    NemadId:<input type="text" name="nid">
+    NamadName:<input type="text" name="nname">
+    <input type="submit" value="Insert" name="btn3">
+</form>
+
+<?php
+
+include_once 'dbcon.php';
+if (isset($_POST['nid']) && isset($_POST['nname']))
+ {
+    $nid =  $_POST['nid']; 
+    $nname =  $_POST['nname'];
+    echo   $nid."  ".  $nname;
+
+    mysqli_query($db,"INSERT INTO `nemad`(`nemadid`, `namadname`) VALUES ('$nid','$nname')");
+ }
+?>
